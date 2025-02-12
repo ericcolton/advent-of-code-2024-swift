036ab1dd0e87bc4779209063ce9747632e3e3521
Eric Colton <ericcolton@gmail.com>
2025-02-12 08:10:05 -0500

    2024 day 5 more work

---
+++ Sources/Day05.swift
@@ -0,0 +1,58 @@
+import Algorithms
+
+struct Day05: AdventDay {
+  
+  typealias InputDataType = (pageOrderings: [(a: Int, b: Int)], updates: [[Int]])
+  
+  let inputData : InputDataType
+  
+  init(data: String) {
+    inputData = Self.parseInputData(rawData: data)
+  }
+  
+  private static func parseInputData(rawData: String) -> InputDataType {
+    let sections = rawData.split(separator: "\n\n")
+    
+    var ordering : [Int:[Int]] = [:]
+    // page ordering rules
+    for pageOrderingRule in sections[0].split(separator: "\n") {
+      let parts = pageOrderingRule.split(separator:"|")
+      let first = parts[0]
+      let second = parts[1]
+      if ordering[first] {
+        
+      }
+      
+      
+      
+
+    }
+    
+    return (pageOrderings: [], updates: [])
+    // rawData.split(separator: "\n").compactMap {
+    //   let components = $0.split(separator: " ")
+    //   if components.count == 2,
+    //      let a = Int(components[0]),
+    //      let b = Int(components[1]) {
+    //     return (a, b)
+    //   }
+    //   return nil
+  }
+
+  func part1() -> Int {
+    print(inputData)
+//    let listA = inputData.map { $0.a }.sorted()
+//    let listB = inputData.map { $0.b }.sorted()
+//    return zip(listA, listB).reduce(0) { $0 + abs($1.0 - $1.1) }
+    return 0
+  }
+
+  func part2() -> Int {
+    return 0
+//    let listA = inputData.map { $0.a }
+//    let counterB = Self.buildCounter(inputData.map { $0.b })
+//    return listA.reduce(into: 0) {
+//      $0 += $1 * (counterB[$1] ?? 0)
+//    }
+  }
+}
