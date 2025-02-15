cf96d1fde8afe2f39cb0fdc5ab95b15ae71216fc
Eric Colton <ericcolton@gmail.com>
2025-02-15 07:10:44 -0500

    2024 day 5 part 1 passes test

--- Sources/Day05.swift
+++ Sources/Day05.swift
@@ -1,8 +1,9 @@
 import Algorithms
 
 struct Day05: AdventDay {
-  
-  typealias InputDataType = (pageOrderings: [Int:Set<Int>], updates: [[Int]])
+
+  typealias PageOrderingRulesType = [Int:Set<Int>]
+  typealias InputDataType = (pageOrderingRules: PageOrderingRulesType, updates: [[Int]])
   
   let inputData : InputDataType
   
@@ -14,15 +15,15 @@ struct Day05: AdventDay {
     let sections = rawData.split(separator: "\n\n")
 
     // parse page ordering rules
-    var pageOrderingRules : [Int:Set<Int>] = [:]
+    var rules : PageOrderingRulesType = [:]
 
     for pageOrderingRule in sections[0].split(separator: "\n") {
       let parts = pageOrderingRule.split(separator:"|")
       if let first = Int(parts[0]), let second = Int(parts[1]) {
-        if pageOrderingRules[first] == nil {
-          pageOrderingRules[first] = Set<Int>()
+        if rules[first] == nil {
+          rules[first] = Set<Int>()
         }
-        pageOrderingRules[first]!.insert(second)
+        rules[first]!.insert(second)
       } else {
         // parse error
         assert(false)
@@ -31,28 +32,48 @@ struct Day05: AdventDay {
     
     // parse updates
     let updates = sections[1].split(separator: "\n").compactMap() {
-      $0.split(separator: ",").compactMap() {
-        Int($0)
-      }
+      $0.split(separator: ",").compactMap() { Int($0) }
     }
 
-    return (pageOrderings: pageOrderingRules, updates: updates)
-    // rawData.split(separator: "\n").compactMap {
-    //   let components = $0.split(separator: " ")
-    //   if components.count == 2,
-    //      let a = Int(components[0]),
-    //      let b = Int(components[1]) {
-    //     return (a, b)
-    //   }
-    //   return nil
+    return (pageOrderingRules: rules, updates: updates)
+  }
+  
+  func entryFollowsRules(first: Int, then: Int) -> Bool {
+    guard let val = inputData.pageOrderingRules[first] else {
+      return false
+    }
+    
+    if val.contains(then) {
+      return true
+    }
+    
+    for subNode in val {
+      if self.entryFollowsRules(first: subNode, then: then) {
+        return true
+      }
+    }
+    return false
+  }
+  
+  func rowFollowsRules(_ row: [Int]) -> Bool {
+    for i in 0..<(row.count - 1) {
+      if !self.entryFollowsRules(first:row[i], then:row[i+1]) {
+        return false
+      }
+    }
+    return true
+  }
+  
+  func findRowValue(_ row: [Int]) -> Int {
+    self.rowFollowsRules(row)
+    ? row[Int(row.count / 2)]
+    : 0
   }
 
   func part1() -> Int {
-    print(inputData)
-//    let listA = inputData.map { $0.a }.sorted()
-//    let listB = inputData.map { $0.b }.sorted()
-//    return zip(listA, listB).reduce(0) { $0 + abs($1.0 - $1.1) }
-    return 0
+    return inputData.updates
+      .map() { self.findRowValue($0) }
+      .reduce(0, +)
   }
 
   func part2() -> Int {
