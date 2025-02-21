d58584cc4e9bbaa5c5bcf919622901e101139239
Eric Colton <ericcolton@gmail.com>
2025-02-21 07:28:15 -0500

    2024 day 6 part 2 partial completion

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -8,6 +8,10 @@ enum Direction: Int {
   }
 }
 
+enum CannotAdvanceReason {
+  case outOfBounds, endlessLoop
+}
+
 struct Coordinate: Hashable {
   let y: Int
   let x: Int
@@ -16,17 +20,21 @@ struct Coordinate: Hashable {
 class Guard {
   let grid : [[Bool]]
   let bounds: (y: Int, x: Int)
+  let start : Coordinate
   var location : Coordinate
   var direction : Direction = .up
-  var seen : Set<Coordinate>
+  var seen : [Coordinate:Set<Direction>]
   var uniqueSteps: Int { seen.count }
+  var rotationCount : Int
   
   init(start: Coordinate, grid: [[Bool]]) {
     assert (grid.count > 0)
     self.grid = grid
     self.bounds = (y: grid.count - 1, x: grid[0].count - 1)
+    self.start = start
     self.location = start
-    self.seen = Set()
+    self.seen = [:]
+    self.rotationCount = 0
   }
   
   private func inBounds(_ point: Coordinate) -> Bool {
@@ -47,15 +55,35 @@ class Guard {
     return !self.inBounds(nextLoc) || !self.grid[nextLoc.y][nextLoc.x]
   }
   
-  func advandeUntilOutofBounds() {
+  private func hasFallenIntoEndlessLoop() -> Bool {
+    seen[location] != nil && seen[location]!.contains(direction)
+  }
+  
+  func advandeWhilePossible() -> CannotAdvanceReason {
     while self.inBounds(location) {
       if self.canAdvance() {
-        seen.insert(location)
+        rotationCount = 0
+        if seen[location] == nil {
+          seen[location] = Set<Direction>()
+        }
+        seen[location]!.insert(direction)
         location = self.nextLocation()
+        if self.hasFallenIntoEndlessLoop() {
+          return .endlessLoop
+        }
       } else {
         direction = direction.rotateRight()
+        if rotationCount == 4 {
+          return .endlessLoop
+        }
+        rotationCount += 1
       }
     }
+    return .outOfBounds
+  }
+  
+  func visitedPoints() -> [Coordinate] {
+    seen.keys.filter() { $0 != start }
   }
 }
 
@@ -80,14 +108,34 @@ struct Day06: AdventDay {
     return (start:start!, grid:grid)
   }
   
+  private func countAddedBarriersThatLeadToEndlessLoops(barriers: [Coordinate]) -> Int {
+    barriers.enumerated().filter{ (i, point) in
+    var modifiedGrid = inputData.grid
+
+
+    modifiedGrid[point.y][point.x] = true
+    let c = modifiedGrid.map() { $0.filter { $0 }.count }
+      .reduce(0, +)
+
+    let aGuard = Guard(start: inputData.start, grid: modifiedGrid)
+    let endless = aGuard.advandeWhilePossible() == .endlessLoop
+    print(i, c, endless)
+    return endless
+    //return aGuard.advandeWhilePossible() == .endlessLoop
+    }.count
+  }
 
   func part1() -> Int {
     let theGuard = Guard(start: inputData.start, grid: inputData.grid)
-    theGuard.advandeUntilOutofBounds()
+    let reason = theGuard.advandeWhilePossible()
+    assert(reason == .outOfBounds)
     return theGuard.uniqueSteps
   }
 
   func part2() -> Int {
-    return 0
+    let theGuard = Guard(start: inputData.start, grid: inputData.grid)
+    let reason = theGuard.advandeWhilePossible()
+    assert(reason == .outOfBounds)
+    return countAddedBarriersThatLeadToEndlessLoops(barriers: theGuard.visitedPoints())
   }
 }
